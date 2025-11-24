f99581bdb528f9d4dcb069576019e643ef83d971
Minuck Choi <54134687+AdHoc2675@users.noreply.github.com>
2025-11-24 18:53:14 +0900

    Spawn System update

--- WinApi/CSceneStage01.cpp
+++ WinApi/CSceneStage01.cpp
@@ -9,6 +9,8 @@
 #include "CPanel.h"
 #include "CButton.h"
 #include "CWeapon.h"
+#include "CCollisionManager.h"
+#include "CExpOrb.h"
 
 CSceneStage01::CSceneStage01()
 {
@@ -20,82 +22,75 @@ CSceneStage01::~CSceneStage01()
 
 void CSceneStage01::Init()
 {
-	CPlayer* player = new CPlayer();
-	player->SetPos(Vec2(CGame::WINSIZE.x * 0.5f, CGame::WINSIZE.y * 0.5f));
-	AddGameObject(player);
+    player = new CPlayer();
+    player->SetPos(Vec2(CGame::WINSIZE.x * 0.5f, CGame::WINSIZE.y * 0.5f));
+    AddGameObject(player);
 
-	CMonster* monster = new CMonster();
-	monster->SetPos(Vec2(CGame::WINSIZE.x * 0.5f, 100));
-	monster->SetPlayer(player);
-	AddGameObject(monster);
+    // 초기에 한 마리 정도
+    CMonster* monster = new CMonster();
+    monster->SetPos(Vec2(CGame::WINSIZE.x * 0.5f, 100));
+    monster->SetPlayer(player);
+    AddGameObject(monster);
 
-	CWeapon* weapon = new CWeapon();
-	player->AddChild(weapon);
-	weapon->SetPlayer(player);
+    CWeapon* weapon = new CWeapon();
+    player->AddChild(weapon);
+    weapon->SetPlayer(player);
 
-	CCameraController* controller = new CCameraController();
-	// 카메라가 따라갈 플레이어를 전달
-	controller->SetPlayer(player);
-	AddGameObject(controller);
+    CCameraController* controller = new CCameraController();
+    controller->SetPlayer(player);
+    AddGameObject(controller);
 
-	CSoundController* sound = new CSoundController();
-	AddGameObject(sound);
+    CSoundController* sound = new CSoundController();
+    AddGameObject(sound);
 
-	CPanel* panel1 = new CPanel();
-	panel1->SetScale(Vec2(200, 200));
-	panel1->SetPos(Vec2(100, 100));
-	AddUI(panel1);
+    // UI 예시 생략(존재 유지)
+    // ...
 
-	auto click1 = [](DWORD_PTR button1, DWORD_PTR param2) {
-		CButton* button = (CButton*)button1;
-		wstring text = button->GetName() + TEXT("이 클릭됨");
-		Logger::Debug(text);
-	};
+    CCollisionManager::GetInstance()->CheckLayer(Layer::Player, Layer::ExpOrb);
+    CCollisionManager::GetInstance()->CheckLayer(Layer::Missile, Layer::Monster);
+    CCollisionManager::GetInstance()->CheckLayer(Layer::Player, Layer::Monster);
 
-	CButton* button1 = new CButton();
-	button1->SetScale(Vec2(100, 50));
-	button1->SetPos(Vec2(50, 50));
-	button1->SetName(TEXT("버튼1"));
-	button1->SetClickCallback(click1, (DWORD_PTR)button1, 0);
-	panel1->AddChild(button1);
-
-	CPanel* panel2 = new CPanel();
-	panel2->SetScale(Vec2(200, 200));
-	panel2->SetPos(Vec2(300, 100));
-	AddUI(panel2);
-
-	auto click2 = [](DWORD_PTR panel1, DWORD_PTR param2) {
-		CPanel* panel = (CPanel*)panel1;
-		EVENT->ShowUI(panel, !panel->IsShow());
-	};
-
-	CButton* button2 = new CButton();
-	button2->SetScale(Vec2(100, 50));
-	button2->SetPos(Vec2(50, 50));
-	button2->SetClickCallback(click2, (DWORD_PTR)panel1, 0);
-	panel2->AddChild(button2);
-
-	CCollisionManager::GetInstance()->CheckLayer(Layer::Player, Layer::ExpOrb);
+    spawnTimer = spawnInterval;
 }
 
 void CSceneStage01::Enter()
 {
-	CAMERA->FadeIn(0.5f);
+    CAMERA->FadeIn(0.5f);
 }
 
 void CSceneStage01::Update()
 {
-	if (INPUT->ButtonDown(VK_ESCAPE))
-	{
-		CAMERA->FadeOut(0.5f);
-		EVENT->ChangeScene(SceneType::Title, 0.5f);
-	}
+    if (INPUT->ButtonDown(VK_ESCAPE))
+    {
+        CAMERA->FadeOut(0.5f);
+        EVENT->ChangeScene(SceneType::Title, 0.5f);
+        return;
+    }
+
+    // 몬스터 지속 스폰
+    spawnTimer -= DT;
+    if (spawnTimer <= 0.f)
+    {
+        // 현재 몬스터 수 제한
+        int monsterCount = 0;
+
+        if (monsterCount < maxMonsters)
+        {
+            SpawnMonster();
+        }
+
+        // 난이도 증가(선택): 간격 서서히 감소
+        spawnInterval -= 0.02f;
+        if (spawnInterval < 1.2f) spawnInterval = 1.2f;
+
+        spawnTimer = spawnInterval;
+    }
 }
 
 void CSceneStage01::Render()
 {
-	Vec2 startPos = CAMERA->WorldToScreenPoint(Vec2(0, 0));
-	Vec2 endPos = CAMERA->WorldToScreenPoint(Vec2(CGame::WINSIZE.x, CGame::WINSIZE.y));
+    Vec2 startPos = CAMERA->WorldToScreenPoint(Vec2(0, 0));
+    Vec2 endPos = CAMERA->WorldToScreenPoint(Vec2(CGame::WINSIZE.x, CGame::WINSIZE.y));
 }
 
 void CSceneStage01::Exit()
@@ -104,4 +99,49 @@ void CSceneStage01::Exit()
 
 void CSceneStage01::Release()
 {
+}
+
+void CSceneStage01::SpawnMonster()
+{
+    if (!player)
+        return;
+
+    Vec2 spawnPos = GetOffScreenSpawnPos();
+
+    CMonster* monster = new CMonster();
+    monster->SetPos(spawnPos);
+    monster->SetPlayer(player);
+    AddGameObject(monster);
+}
+
+Vec2 CSceneStage01::GetOffScreenSpawnPos() const
+{
+    // 화면 크기
+    Vec2 size = CGame::WINSIZE;
+
+    // 0: 위, 1: 아래, 2: 좌, 3: 우 중 하나 선택
+    int side = rand() % 4;
+
+    Vec2 pos;
+    switch (side)
+    {
+    case 0: // 위
+        pos.x = (float)(rand() % (int)size.x);
+        pos.y = -offScreenMargin;
+        break;
+    case 1: // 아래
+        pos.x = (float)(rand() % (int)size.x);
+        pos.y = size.y + offScreenMargin;
+        break;
+    case 2: // 좌
+        pos.x = -offScreenMargin;
+        pos.y = (float)(rand() % (int)size.y);
+        break;
+    case 3: // 우
+    default:
+        pos.x = size.x + offScreenMargin;
+        pos.y = (float)(rand() % (int)size.y);
+        break;
+    }
+    return pos;
 }
--- WinApi/CSceneStage01.h
+++ WinApi/CSceneStage01.h
@@ -1,16 +1,30 @@
 #pragma once
+class CPlayer;
+
 class CSceneStage01 : public CScene
 {
 public:
-	CSceneStage01();
-	virtual ~CSceneStage01();
+    CSceneStage01();
+    virtual ~CSceneStage01();
 
 public:
-	void Init()		override;
-	void Enter()	override;
-	void Update()	override;
-	void Render()	override;
-	void Exit()		override;
-	void Release()	override;
+    void Init()     override;
+    void Enter()    override;
+    void Update()   override;
+    void Render()   override;
+    void Exit()     override;
+    void Release()  override;
+
+private:
+    void SpawnMonster();                 // 몬스터 1마리 스폰
+    Vec2 GetOffScreenSpawnPos() const;   // 화면 밖 위치 계산
+
+private:
+    CPlayer* player = nullptr;           // 플레이어 참조
+    float    spawnInterval = 3.f;        // 기본 스폰 주기(초)
+    float    spawnTimer    = 0.f;        // 타이머
+	int      currentMonsterCount = 0;    // 현재 씬 몬스터 수
+    int      maxMonsters   = 15;         // 씬 최대 몬스터
+    float    offScreenMargin = 80.f;     // 화면 밖으로 얼마나 떨어뜨릴지
 };
 
